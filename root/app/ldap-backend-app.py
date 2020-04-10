474eb741318a97c3a92dd53566bf90917a1a2bcd
Philipp Staiger <philipp@staiger.it>
2020-04-10 10:17:09 +0200

    Add Cookie Name, Logout and Path Support
    Signed-off-by: Philipp Staiger <philipp@staiger.it>

--- root/app/ldap-backend-app.py
+++ root/app/ldap-backend-app.py
@@ -41,32 +41,88 @@ class AppHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
 
-        url = urlparse(self.path)
-
-        if url.path.startswith("/login"):
-            return self.auth_form()
-
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(ensure_bytes('Hello, world! Requested URL: ' + self.path + '\n'))
-
-
-    # send login form html
-    def auth_form(self, target = None, cookie_domain = None):
-
         # try to get target location from header
-        if target == None:
-            target = self.headers.get('X-Target')
-
-        # try to get cookie domain from header
-        if cookie_domain == None:
-                cookie_domain = self.headers.get('X-Cookie-Domain')
-
+        target = self.headers.get('X-Target')
         # form cannot be generated if target is unknown
         if target == None:
             self.log_error('target url is not passed')
             self.send_response(500)
             return
+
+        # try to get cookie name from header
+        cookie_name = self.headers.get('X-CookieName')
+        # cookie cannot be set if name is unknown
+        if cookie_name == None:
+            self.log_error('cookie name is not passed')
+            self.send_response(500)
+            return
+
+        # try to get cookie domain from header
+        cookie_domain = self.headers.get('X-Cookie-Domain')
+
+        # try to get login path from header
+        path_login = self.headers.get('X-Path-Login')
+        if path_login == None or !path_login.startswith("/"):
+            path_login = "/login"
+        # try to get logout path from header
+        path_logout = self.headers.get('X-Path-Logout')
+        if path_logout == None or !path_logout.startswith("/"):
+            path_logout = "/logout"
+
+        url = urlparse(self.path)
+
+        if url.path.startswith(path_login):
+            return self.auth_form(target, cookie_name, cookie_domain)
+        if url.path.startswith(path_logout):
+            return self.logout(target, cookie_name, cookie_domain)
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(ensure_bytes('Hello, world! Requested URL: ' + self.path + '\n'))
+
+    # send logout message html and redirect to home.staiger.it
+    def logout(self, target, cookie_name, cookie_domain = None:
+
+        cookie_domain_part = ''
+        if cookie_domain:
+            cookie_domain_part = 'domain=' + cookie_domain + ';'
+
+        html="""
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <meta http-equiv=Content-Type content="text/html;charset=UTF-8">
+        <meta http-equiv="refresh" content="3;url=https://home.staiger.it/" />
+        <title>Log Out</title>
+        <style type="text/css" rel="stylesheet">
+            body { background-color: #f1f1f1; font-family: sans-serif,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif; }
+            .log-in { width: 400px; height: 500px; position: absolute; top: 0; bottom: 0; left: 0; right: 0; margin: auto; background-color: #fff; border-radius: 3px; overflow: hidden; -webkit-box-shadow: 0px 0px 2px 0px rgba(222,222,222,1); -moz-box-shadow: 0px 0px 2px 0px rgba(222,222,222,1); box-shadow: 0px 0px 2px 0px rgba(222,222,222,1); }
+            .log-in > div { position: relative; }
+            .log-in .content { margin-top: 50px; padding: 20px; text-align: center; }
+			.logo { text-align: center; max-height: 150px; }
+            h1, h2 { text-align: center; }
+            h1 {  margin-top: 20px; margin-bottom: 20px; letter-spacing: -0.05rem; color: #565656; font-size: 1.6rem; }
+        </style>
+    </head>
+    <body>
+        <div class="log-in">
+            <div class="content">
+				<img class="logo" src="https://staiger.it/wp-content/uploads/2016/01/cropped-Logo-270x270.png">
+                <h1>You are now logged out</h1>
+            </div>
+        </div>
+    </body>
+</html>"""
+        self.send_response(200)
+        # Proxy Auth
+        self.send_header('Set-Cookie', cookie_name + '=deleted; Max-Age=0; ' + cookie_domain_part + ' httponly')
+        self.send_header('Set-Cookie', 'nginxauth=deleted; Max-Age=0; httponly; Domain=staiger.it')
+        self.end_headers()
+        self.wfile.write(ensure_bytes(html))
+
+
+    # send login form html
+    def auth_form(self, target, cookie_name, cookie_domain = None):
 
         html="""
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -103,6 +159,7 @@ class AppHandler(BaseHTTPRequestHandler):
                         <input type="text" name="token" placeholder="2FA Token" aria-label="2FA Token" />
                     </p> -->
                     <input type="hidden" name="target" value="TARGET">
+                    <input type="hidden" name="cookie_name" value="COOKIE_NAME">
                     <input type="hidden" name="cookie_domain" value="COOKIE_DOMAIN">
                     <button type="submit" class="submit btn btn-primary">Log In</button>
                 </form>
@@ -113,7 +170,7 @@ class AppHandler(BaseHTTPRequestHandler):
 
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(ensure_bytes(html.replace('TARGET', target).replace('COOKIE_DOMAIN', cookie_domain)))
+        self.wfile.write(ensure_bytes(html.replace('TARGET', target).replace('COOKIE_NAME', cookie_name).replace('COOKIE_DOMAIN', cookie_domain)))
 
 
     # processes posted form and sets the cookie with login/password
@@ -131,6 +188,8 @@ class AppHandler(BaseHTTPRequestHandler):
         user = form.getvalue('username')
         passwd = form.getvalue('password')
         target = form.getvalue('target')
+        cookie_name = form.getvalue('cookie_name')
+        cookie_domain = form.getvalue('cookie_domain')
 
         if user != None and passwd != None and target != None:
 
@@ -144,12 +203,11 @@ class AppHandler(BaseHTTPRequestHandler):
             enc = cipher_suite.encrypt(ensure_bytes(user + ':' + passwd))
             enc = enc.decode()
 
-            cookie_domain = os.environ['COOKIE_DOMAIN']
             cookie_domain_part = ''
             if cookie_domain:
                 cookie_domain_part = 'domain=' + cookie_domain + ';'
 
-            self.send_header('Set-Cookie', 'nginxauth=' + enc + '; ' + cookie_domain_part + ' httponly')
+            self.send_header('Set-Cookie', cookie_name + '=' + enc + '; ' + cookie_domain_part + ' httponly')
 
             self.send_header('Location', target)
             self.end_headers()
